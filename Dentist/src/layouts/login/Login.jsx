a496b778be3a33984a4024b0708964decd002bd6
Àlex <alexm0u.13@gmail.com>
2023-03-21 19:52:30 +0100

    FIX: Login layout fixed

--- Dentist/src/layouts/login/Login.jsx
+++ Dentist/src/layouts/login/Login.jsx
@@ -1,99 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import NavBar from '../../common/Navbar/NavBar'
-import { InputText } from '../../common/InputText/InputText';
-import { validate } from '../../helpers/useful';
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import React, { useState, useEffect }  from 'react';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import NavBar from '../../common/Navbar/NavBar';
 
-export const Login = () => {
-    // const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const credentialsRdx = useSelector(userData);
+export function Login() {
+    let user = {
+        email: '',
+        password: ''
+    };
+    const [valor, setValor] = useState(user);
+    const { email, password} = valor;
+    const newValue = ({target}) => {
 
-
-    // const credentialsRdx = useSelector(userData);
-    const [welcome, setWelcome] = useState("");
-    const [credenciales, setCredenciales] = useState({
-    email: '',
-    password: ''
-    })
-
-    // useEffect(() => {
-    //     if (credentialsRdx.credentials.token) {
-    //       //Si No token...home redirect
-    //       Navigate("/");
-    //     }
-    //   }, []);
-
-    const inputHandler = (e) => {
-        setCredenciales((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-  }
-
-  const checkError = (e) => {
-  }
-
-  const logeame = () => {
-    logMe(credenciales)
-        .then( respuesta => {
-              let decodificado = decodeToken(respuesta.data)
-              console.log("este es el  token decodificado", decodificado)
-                let datosBackend = {
-                    token: respuesta.data,
-                    usuario: decodificado
-                };       
-                //Este es el momento en el que guardo en REDUX
-                // console.log("este es el token decofdificado", decodificado)
-                // console.log("este es datosBacked", datosBackend)
-                // console.log("este es respuesta", respuesta)
-                dispatch(login({credentials: datosBackend}));
-                setWelcome(`Bienvenid@ de nuevo ${datosBackend.usuario.fullName}`);
-        //Redirección a Home
-        // setTimeout(() => {
-        //     navigate("/login");
-        //   }, 3000);
-        })
-        .catch(error => console.log(error))
-  };
+        const {name, value} = target;
+        setValor({...valor,[name]:value});
+    }
     return (
         <>
         <NavBar />
-        <hr />
         <div style={{ display: 'block', width: 700, padding: 30 }}>
-            <div className="loginDesign">
-                {welcome !== "" ? (
-            <div>{welcome}</div>
-        ) : (
-            <div>
-                <Container>
-                    <Row className="LoginForm">
-                        <Col lg={6}>
-                            <Form>
-                                <Form.Group>
-                                    <Form.Label>Enter your email account:</Form.Label>
-                                    <InputText className={"inputLogin"}
-                                    type={"email"} name={"email"} placeholder={"email..."} required={true}
-                                    changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Enter your password:</Form.Label>
-                                    <InputText className={"inputLogin"}
-                                    type={"password"} name={"password"} placeholder={""} required={true} 
-                                    changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
-                                </Form.Group>
-                                <br />
-                                <Button className="botonLog" variant="primary" 
-                                onClick={()=> logeame()}> Login User
-                                </Button>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Container>
+            <div className='formulario'>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control type="email"  name="email" placeholder="Email..."  value={email} onChange={newValue}/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password" name="password" placeholder="Password..." value={password} onChange={newValue} />
+                    </Form.Group>
+                    <br />
+                    <div className='botones'>
+                        <Button variant="primary" className='botonLogin' style={{ width: "5em", height: "1.9em",}}  type="submit">
+                        Log Me
+                        </Button>
+                    </div>
+                </Form>
             </div>
-        )}
-        </div>
-
         </div>
         </>
     );
