c2b8c0c2ea5627da7b3147344e98bd268c7bb2b8
Alexm0u <alexm0u.13@gmail.com>
2023-03-22 13:44:44 +0100

    FEAT: Modified login Layout, first attempt with token and credentials

--- Dentist/src/layouts/login/Login.jsx
+++ Dentist/src/layouts/login/Login.jsx
@@ -2,19 +2,92 @@ import React, { useState, useEffect }  from 'react';
 import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import NavBar from '../../common/Navbar/NavBar';
+import { InputText } from '../../common/InputText/InputText';
+import { validate } from "../../helpers/useful";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
-    let user = {
-        email: '',
-        password: ''
-    };
-    const [valor, setValor] = useState(user);
-    const { email, password} = valor;
-    const newValue = ({target}) => {
+    const navigate = useNavigate();
+    const [credenciales, setCredenciales] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+    });
 
-        const {name, value} = target;
-        setValor({...valor,[name]:value});
-    }
+    const [credencialesError, setCredencialesError] = useState({
+        fullNameError: "",
+        emailError: "",
+        passwordError: "",
+    });
+
+    const [valiUser, setValiUser] = useState({
+        fullNameVali: false,
+        emailVali: false,
+        passwordVali: false,
+    });
+
+    const [LoginAct, setLoginAct] = useState(false);
+
+    const inputHandler = (e) => {
+        setCredenciales((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    useEffect(() => {
+        for (let error in credencialesError) {
+            if (credencialesError[error] !== "") {
+                setLoginAct(false);
+                return;
+            }
+        }
+        for (let vacio in credenciales) {
+            if (credenciales[vacio] === "") {
+                setLoginAct(false);
+                return;
+            }
+        }
+        for (let validated in valiUser) {
+            if (valiUser[validated] === false) {
+                setLoginAct(false);
+                return;
+            }
+        }
+        setLoginAct(true);
+    });
+
+    const checkError = (e) => {
+        let error = "";
+        const checked = validate(
+            e.target.name,
+            e.target.value,
+            e.target.required
+        );
+        error = checked.message;
+
+        setValiUser((prevState) => ({
+            ...prevState,
+            [e.target.name + "Vali"]: checked.validated,
+        }));
+
+        setCredencialesError((prevState) => ({
+            ...prevState,
+            [e.target.name + "Error"]: error,
+        }));
+    };
+
+    const loginUser = () => {
+        newUser(credenciales)
+            .then(() => {
+                setTimeout(() => {
+                    navigate("/profile");
+                }, 2000);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    
     return (
         <>
         <NavBar />
@@ -23,11 +96,36 @@ export function Login() {
                 <Form>
                     <Form.Group>
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email"  name="email" placeholder="Email..."  value={email} onChange={newValue}/>
+                        <InputText
+                                        className={
+                                            credencialesError.emailError === ""
+                                                ? "inputBasicDesign"
+                                                : "inputBasicDesign inputErrorDesign"
+                                        }
+                                        type={"email"}
+                                        name={"email"}
+                                        placeholder={"Email..."}
+                                        required={true}
+                                        changeFunction={(e) => inputHandler(e)}
+                                        blurFunction={(e) => checkError(e)}
+                                    />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Password:</Form.Label>
-                        <Form.Control type="password" name="password" placeholder="Password..." value={password} onChange={newValue} />
+                        <InputText
+                                        className={
+                                            credencialesError.passwordError ===
+                                            ""
+                                                ? "inputBasicDesign"
+                                                : "inputBasicDesign inputErrorDesign"
+                                        }
+                                        type={"password"}
+                                        name={"password"}
+                                        placeholder={"Password..."}
+                                        required={true}
+                                        changeFunction={(e) => inputHandler(e)}
+                                        blurFunction={(e) => checkError(e)}
+                                    />
                     </Form.Group>
                     <br />
                     <div className='botones'>
